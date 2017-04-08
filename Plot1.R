55b341d0586ade87f863d8f05eeb4a5454f1c09a
amitozarkar <amit.ozarkar@gmail.com>
2017-04-08 15:44:09 +0530

    coursera-eda-project-1

--- Plot1.R
+++ Plot1.R
@@ -4,6 +4,7 @@ EPC <- read.table("C:/Amit/DataScience/Coursera/ED/household_power_consumption.t
 EPC_subset <-EPC[EPC$Date %in% c("1/2/2007","2/2/2007"),]
 # as all columns are of character type, need to convert GlobalActivePower to Numeric
 GlobalActivePower <- as.numeric(EPC_subset$Global_active_power)
-png("plot1.png",width = 480,height = 480)
 hist(GlobalActivePower,col="red",main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab = "Frequency")
+#allows to first see the graph on monitor, then you can copy to png file
+dev.copy(png,"plot1.png",width = 480,height = 480)
 dev.off()
